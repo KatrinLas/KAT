9d35d49de43fc4b7050a805c6061530008ba6578
Katrin Lasberg <klasberg@itcollege.ee>
2016-11-26 22:14:04 +0200

    katse2

---
+++ src/com/helloworld.java
@@ -0,0 +1,10 @@
+package com;
+
+/**
+ * Created by Lenovo on 26.11.2016.
+ */
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        }
+    }
